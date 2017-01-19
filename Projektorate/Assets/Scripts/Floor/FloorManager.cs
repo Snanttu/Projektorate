c729f1e179a1342177474c0b1f2c009e85d92166
LENOVO-PC\santeri <santeri.orava@hotmail.com>
2017-01-19 13:16:27 +0200

    All sorts'a shiii

--- Projektorate/Assets/Scripts/Floor/FloorManager.cs
+++ Projektorate/Assets/Scripts/Floor/FloorManager.cs
@@ -3,13 +3,19 @@ using System.Collections;
 
 public class FloorManager : MonoBehaviour {
 
-    public float _speed = 0.1F;
+    public float _speedStart;
+    public float _speed;
+    public float _speedIncrease;
+    public float _speedMax;
     public GameObject[] _floors;
+    public GameObject _enemy;
     public float _startPosition;
     public float _endPosition;
 
     // Use this for initialization
     void Start () {
+        _speed = _speedStart;
+
         for (int i = 0; i < 10; i++) {
             if (i < 4) {
                 Instantiate(_floors[0], new Vector3(0, 0, i * 5.0f), Quaternion.identity);
@@ -27,6 +33,13 @@ public class FloorManager : MonoBehaviour {
 
     public void CreateFloor()
     {
+        float _random = Random.Range(0, 10);
         Instantiate(_floors[Random.Range(1, 5)], new Vector3(0, 0, _startPosition), Quaternion.identity);
+        if (_random == 5) {
+            Instantiate(_enemy, new Vector3(0, 0, _startPosition), Quaternion.identity);
+        }
+        if (_speed < _speedMax) {
+            _speed += _speedIncrease;
+        }
     }
 }
---
+++ Projektorate/Assets/Scripts/Obstacles/Enemy.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    public float m_fBounceDuration;
+    public Vector3 m_vStartPos;
+    public Vector3 m_vEndPos;
+
+    private Transform m_gcTransform;
+    private float m_fEventTime;
+
+    private GameObject _floorManager;
+    private FloorManager _floorManagerScript;
+    private float _speed;
+
+    // Use this for initialization
+    void Start()
+    {
+        _floorManager = GameObject.Find("FloorManager");
+        _floorManagerScript = _floorManager.GetComponent<FloorManager>();
+        _speed = _floorManagerScript._speed;
+        m_gcTransform = GetComponent<Transform>();
+        m_fEventTime = Time.time;
+        m_gcTransform.position = m_vStartPos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float fRatio = (Time.time - m_fEventTime) / m_fBounceDuration;
+        m_vStartPos.z *= _speed * Time.deltaTime;
+        m_vEndPos.z *= _speed * Time.deltaTime;
+
+        Vector3 vPos = Vector3.Lerp(m_vStartPos, m_vEndPos, fRatio);
+        //Easing.EaseInOut(fRatio, EasingType.Sine, EasingType.Sine);
+        vPos.z = m_gcTransform.position.z;
+        m_gcTransform.position = vPos;
+
+        if (fRatio >= 1.0f)
+        {
+            FlipDirection();
+            m_fEventTime = Time.time;
+        }
+    }
+
+    public void FlipDirection()
+    {
+        Vector3 vTemp = m_vStartPos;
+        m_vStartPos = m_vEndPos;
+        m_vEndPos = vTemp;
+    }
+}
